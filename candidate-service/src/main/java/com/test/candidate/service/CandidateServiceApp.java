008f2dbca88d9fa2ec93ab7675a74e47921e1298
edigitman <edi.gitman@gmail.com>
2015-09-11 10:34:21 +0200

    declared port and remote server as static final variables.

--- candidate-service/src/main/java/com/test/candidate/service/CandidateServiceApp.java
+++ candidate-service/src/main/java/com/test/candidate/service/CandidateServiceApp.java
@@ -2,14 +2,12 @@ package com.test.candidate.service;
 
 import com.test.candidate.persistence.PersistenceConfiguration;
 import com.test.candidate.service.remote.IntakeGenerationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
 import org.springframework.remoting.rmi.RmiProxyFactoryBean;
-import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
 import org.springframework.remoting.rmi.RmiServiceExporter;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -24,6 +22,8 @@ public class CandidateServiceApp {
         SpringApplication.run(CandidateServiceApp.class, args);
     }
 
+    public static final int RMI_PORT = 5000;
+    public static final String REMOTE_SERVER = "localhost";
 
     @Bean(name = "myRmiServiceExporter")
     public RmiServiceExporter registerService(IntakeGenerationServiceImpl barService) {
@@ -31,7 +31,7 @@ public class CandidateServiceApp {
         rmiServiceExporter.setServiceName(IntakeGenerationService.RMI_NAME);
         rmiServiceExporter.setService(barService);
         rmiServiceExporter.setServiceInterface(IntakeGenerationService.class);
-        rmiServiceExporter.setRegistryPort(5000);
+        rmiServiceExporter.setRegistryPort(RMI_PORT);
         return rmiServiceExporter;
     }
 
@@ -39,7 +39,7 @@ public class CandidateServiceApp {
     @DependsOn("myRmiServiceExporter")
     public IntakeGenerationService createIntakeGenerationServiceLink() {
         RmiProxyFactoryBean rmiProxyFactoryBean = new RmiProxyFactoryBean();
-        rmiProxyFactoryBean.setServiceUrl("rmi://192.168.1.52:5000/" + IntakeGenerationService.RMI_NAME);
+        rmiProxyFactoryBean.setServiceUrl("rmi://" + REMOTE_SERVER + ":" + RMI_PORT + "/" + IntakeGenerationService.RMI_NAME);
         rmiProxyFactoryBean.setServiceInterface(IntakeGenerationService.class);
         rmiProxyFactoryBean.afterPropertiesSet();
         return (IntakeGenerationService) rmiProxyFactoryBean.getObject();
